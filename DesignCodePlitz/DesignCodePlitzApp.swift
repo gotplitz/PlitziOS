13444547375dd458905bf076a7fda8ebeb751e5c
Norman Pleitez <webmaster@plitzcorporation.com>
2023-03-04 13:13:25 -0500

    Initial Commit

---
+++ DesignCodePlitz/DesignCodePlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodePlitzApp.swift
+//  DesignCodePlitz
+//
+//  Created by Norman Pleitez on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodePlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
