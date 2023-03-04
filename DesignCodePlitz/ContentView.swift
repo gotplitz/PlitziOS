da4c71193733730d517f0ccc9c235ee464fca4a8
Norman Pleitez <webmaster@plitzcorporation.com>
2023-03-04 16:17:12 -0500

    Learned about gestures and animations

--- DesignCodePlitz/ContentView.swift
+++ DesignCodePlitz/ContentView.swift
@@ -8,35 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
+    
     var body: some View {
         ZStack {
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default, value: show)
             
             BackCardView()
-                .background(Color("card4"))
+                .background(show ? Color("card3") : Color("card4"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -40)
+                .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.9)
-                .rotationEffect(.degrees(10))
-                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10, y: 0, z: 0))
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.4), value: show)
             
             BackCardView()
-                .background(Color("card3"))
+                .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
-                .rotationEffect(.degrees(5))
-                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10, y: 0, z: 0))
+                .rotationEffect(.degrees(show ? 0 : 5))
+                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.2), value: show)
             
             CardView()
+                .offset(x: viewState.width, y: viewState.height)
+                .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0), value: viewState)
+                .onTapGesture {
+                    self.show.toggle()
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged {
+                            value in
+                            self.viewState = value.translation
+                            self.show = true
+                        }
+                        .onEnded {
+                            value in
+                            self.viewState = .zero
+                            self.show = false
+                        }
+            )
             
             BottomCardView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default, value: show)
 
         }
     }
@@ -57,6 +84,8 @@ struct CardView: View {
                         .font(.title)
                         .fontWeight(.semibold)
                         .foregroundColor(Color.white)
+                        .frame(width: 200, alignment: .leading)
+                        
                     Text("Certificate")
                         .foregroundColor(Color("accent"))
                 }
